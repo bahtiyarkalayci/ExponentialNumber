cd83e412a9c5c5bcee383e53736f9dcc6293d73f
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-20 20:39:56 +0300

    https://www.patika.dev/tr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
+    static int exponential(int a,int b){
+        int result=1;
+        for (int i=1;i<=b;i++){
+            result*=a;
+
+        }
+        System.out.println("result="+result);
+        return result;
+    }
+
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("enter base number");
+            int a = scanner.nextInt();
+            if(a==0){
+                break;
+            }
+            System.out.println("enter power number");
+            int b = scanner.nextInt();
+            exponential(a, b);
+        }
+
     }
 }
